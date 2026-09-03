502cc311fa5f776caf8ae3e174506fa4beda36af
Erick Bula <bulaerick36@gmail.com>
2026-09-03 17:24:49 -0500

    fix(syntax): corregir etiqueta script faltante en index.html, corregir boton contactanos y actualizar SW a v302

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bulapay-v301';
+const CACHE_NAME = 'bulapay-v302';
 const ASSETS = [
   './',
   './index.html',
@@ -18,7 +18,7 @@ self.addEventListener('install', (e) => {
   self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log('[Service Worker] Caching app shell v301');
+      console.log('[Service Worker] Caching app shell v302');
       return cache.addAll(ASSETS);
     })
   );
